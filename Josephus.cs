78a7bbf581aeb3cfa919a38d6ff1e9e988d870bc
giuliettiseba <32080740+giuliettiseba@users.noreply.github.com>
2017-11-19 04:23:13 -0300

    Add files via upload

---
+++ Josephus.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Josephus
+{
+    class Josephus
+    {
+        public int[] josephusItera(int personas, int k)
+        {
+            //inicializar el circulo con todas las personas vivas representadas con 1
+            int[] circulo = new int[personas];
+            for (int i = 0; i < personas; i++)
+            {
+                circulo[i] = 1;
+            }
+
+            //Comienza el juego
+            int indice = 0;
+            while (personas > 1)
+            {
+                for (int i = 0; i < k; i++)
+                {
+                    // Arreglo circular
+                    if (indice > circulo.Length - 1)
+                    {
+                        indice = 0;
+                    }
+
+                    // Salto soldados muertos
+                    while (circulo[indice] == 0)
+                    {
+                        indice++;
+                        if (indice > circulo.Length - 1)
+                        {
+                            indice = 0;
+                        }
+                    }
+
+                    // Avanzo una posicion del salto.
+                    indice++;
+
+
+                }
+                //Mato al soldado
+                DibujarArreglo(circulo, indice-1);
+                circulo[indice - 1] = 0;
+                personas--;
+                
+                System.Threading.Thread.Sleep(1000);
+            }
+            return circulo;
+        }
+
+        internal void DibujarArreglo(int[] circulo)
+        {
+            DibujarArreglo(circulo, 99999);
+        }
+
+        public void imprimirTitulo()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.WriteLine("|                            Josephus Problem                               |");
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.ResetColor();
+        }
+
+
+
+        private void DibujarArreglo(int[] circulo, int indice)
+        {
+            
+            Console.Clear();
+            imprimirTitulo();
+            Console.SetCursorPosition(0, 4);
+
+            for (int i = 0; i < circulo.Length; i++)
+            {
+
+                if (indice == i) Console.ForegroundColor = ConsoleColor.Yellow;
+                else if (circulo[i] == 1) Console.ForegroundColor = ConsoleColor.Green;
+                else Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[" + i + "] = " + circulo[i]);
+
+            }
+            Console.ResetColor();
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Josephus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Josephus josephus = new Josephus();
+
+            josephus.imprimirTitulo();
+            String input = "";
+            while (!Int32.TryParse(input, out int num))
+            {
+                Console.Write("Ingrese cantidad de personas: ");
+                input = Console.ReadLine();
+            }
+
+
+            
+            // Asigno cantidad de personas en el circulo
+            int personas = Convert.ToInt32(input);
+
+            input = "";
+            while (!Int32.TryParse(input, out int num))
+            {
+                Console.Write("Ingrese salto: ");
+                input = Console.ReadLine();
+            }
+            // Asigno tamaño del salto
+            int k = Convert.ToInt32(input);
+
+
+           
+            int[] circulo = josephus.josephusItera(personas, k);
+
+            josephus.DibujarArreglo(circulo);
+
+            Console.WriteLine("Press a key to exit...");
+            Console.ReadKey();
+        }
+
+       
+
+    }
+}
